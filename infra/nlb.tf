89d773998ae5106fc9b8be0a5eb4396c884ed1b6
HenriqueLorente <120747355+HenriqueLorente@users.noreply.github.com>
2025-08-23 00:04:04 +0000

    chore: create ci/cd infra

--- infra/nlb.tf
+++ infra/nlb.tf
@@ -1,9 +1,15 @@
+# Descobre o VPC a partir da primeira subnet informada
+data "aws_subnet" "first" {
+  id = var.subnets_id[0]
+}
+
 # Crie um Network Load Balancer
 resource "aws_lb" "this" {
   name = format("%s-nlb", var.cluster_name)
 
   subnets            = var.subnets_id
-  security_groups    = [aws_security_group.allow_inbound.id]
+  # NLB não usa Security Group
+  # security_groups    = [aws_security_group.allow_inbound.id]
   load_balancer_type = "network"
 
   tags = {
@@ -28,8 +34,14 @@ resource "aws_lb_target_group" "this" {
   name        = format("%s-tg", var.cluster_name)
   port        = 80
   protocol    = "TCP"
-  vpc_id      = var.vpc_id
   target_type = "ip"
+  vpc_id      = data.aws_subnet.first.vpc_id  # <-- derivado das subnets
+
+  # (opcional, mas recomendado)
+  # health_check {
+  #   protocol = "TCP"
+  #   port     = "traffic-port"
+  # }
 
   tags = {
     Name = format("%s-tg", var.cluster_name)
